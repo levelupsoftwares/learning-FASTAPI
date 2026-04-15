740525cd57019ce33180016c3cee4630d987f366
Usman Butt <usmanbutt2357@gmail.com>
2026-04-15 19:26:59 +0500

    learning: exparimenting with fastapi

---
+++ 1_MiniProject/api.py
@@ -0,0 +1,32 @@
+import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def load_data():
+    with open('1_MiniProject/patients.json' ,'r') as f:
+       return json.load(f) 
+
+@app.get('/')
+def home():
+    return 'Welcome to the website '
+
+@app.get('/contact')
+def contact():
+    return 'contact us on example.com'
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data 
+
+
+# dyncamicaly access the data
+@app.get('/view/{patient_id}')
+def patient(patient_id:str):
+    data = load_data()
+    for patient in data:
+        if patient["patient_id"] == patient_id:
+            return patient
+    return {'error':'data not avalible'}
+            
