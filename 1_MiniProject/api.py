0ed316ab8eb361f46c4980273588f555a042fa6c
Usman Butt <usmanbutt2357@gmail.com>
2026-04-20 19:37:49 +0500

    lhand's-on Query parameter by implementing "sort" endpoint

--- 1_MiniProject/api.py
+++ 1_MiniProject/api.py
@@ -1,5 +1,5 @@
 import json
-from fastapi import FastAPI ,Path ,HTTPException
+from fastapi import FastAPI ,Path ,HTTPException, Query
 
 app = FastAPI()
 
@@ -28,5 +28,35 @@ def patient(patient_id:str = Path(...,description='ID of the patient in the DB',
     for patient in data:
         if patient["patient_id"] == patient_id:
             return patient
-    raise HTTPException(status_code=404, detail="patient data don't exist")
+    raise HTTPException(status_code=400, detail="patient data don't exist")
             
+# using Query parameter 
+
+@app.get('/sort')
+def sort_patient(sort_by: str = Query(...,description='sort on the basis of age , gender or blood group'),order: str = Query('asc',description='sort in acs or desc order')):
+
+    valid_fields = ['age','gender','blood_group']
+
+    if sort_by.lower() not in valid_fields:
+        raise HTTPException(status_code=400
+                           ,detail=f'invalid input:plz select only from {valid_fields}'
+                           )
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,
+                            detail='Invalid order: select only from asc and desc ')
+    
+    data = load_data()
+    # for patient in data:
+    sort_order = True if order == 'desc' else False
+    def get_value(patient):
+             if sort_by == 'age':
+                return patient['personal_info'].get('age', 0)
+             elif sort_by == 'gender':
+                return patient['personal_info'].get('gender', '')
+             elif sort_by == 'blood_group':
+                return patient['medical_info'].get('blood_group', '')
+    sorted_data = sorted(data, key=get_value, reverse=sort_order)
+
+    return sorted_data
+    
